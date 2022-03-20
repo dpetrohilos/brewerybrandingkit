844bf9a8a42aee9f8a37e3372456a0cf41401978
dpetrohilos <dylanpetrohilos@gmail.com>
2022-03-19 20:23:21 -0400

    beta

--- src/config.rb
+++ src/config.rb
@@ -1,6 +1,6 @@
 http_path = "/" #root level target path
 css_dir = "." #targets our default style.css file at the root level of our theme
-sass_dir = "css/sass" #targets our sass directory
+sass_dir = "css" #targets our sass directory
 images_dir = "img" #targets our pre existing image directory
 javascripts_dir = "js" #targets our JavaScript directory
  
